b73e8dde5188440a37aa0436e006f2347aae2e15
sanketk <42690425+sanketarm@users.noreply.github.com>
2020-10-28 17:27:01 +0530

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,10 +1,10 @@
 module top;
       import uvm_pkg::*;
-      import example_pkg::*;
+      //import example_pkg::*;
       
 dut_if dut_if1();
 
-dut dut1(.DATAOUT(dut_if1.DATAOUT), .full(dut_if1.full),.empty(dut_if1.empty), .clock(dut_if1.clock), .reset(dut_if1.reset), .wn(dut_if1.wn),.rn(dut_if1.rn),.DATAIN(dut_if1.DATAIN))
+dut dut1(.DATAOUT(dut_if1.DATAOUT), .full(dut_if1.full),.empty(dut_if1.empty), .clock(dut_if1.clock), .reset(dut_if1.reset), .wn(dut_if1.wn),.rn(dut_if1.rn),.DATAIN(dut_if1.DATAIN));
 
 
 initial 
@@ -14,7 +14,7 @@ begin
 
 uvm_config_db#(virtual dut_if)::set(null, "*", "dut_vif" , dut_if1);
 
-run_test() // test provided by +UVM_TESTNAME=
+      run_test(); // test provided by +UVM_TESTNAME=
 
 end
 endmodule 
