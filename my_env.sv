5e9a1f0a1fc17a3781e54892fa08b08d5e5352ca
sanketk <42690425+sanketarm@users.noreply.github.com>
2020-10-28 18:28:53 +0530

    Update my_env.sv

--- my_env.sv
+++ my_env.sv
@@ -15,13 +15,13 @@ my_agent_config_h = my_agent_config :: type_id :: create("my_agent_config");
 //uvm_config_db#(my_agent_config_h)::set(null,"*","my_agent_config",my_agent_config_h);
 
 my_agent_h = my_agent :: type_id :: create("my_agent",this);
-my_subscriber_h = my_subscriber ::type_id :: create("my_subsciber",this);
+    my_subscriber_h = my_subscriber ::type_id :: create("my_subscriber",this);
 
 endfunction:build_phase
 
 function void connect_phase(uvm_phase phase);
 
-my_agent_h.aport.connect(my_subsciber.analysis_port);
+  my_agent_h.aport.connect(my_subscriber_h.analysis_export);
 
 endfunction:connect_phase
 
